8b4b92c1ed9a5650cd2f6a116abf257df358cd09
Tinamiru <sjf95@naver.com>
2022-06-14 19:38:00 +0900

    Java Network TCP Exam

---
+++ src/kr/or/ddit/tcp/TcpChatClient.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.tcp;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class TcpChatClient {
+    public static void main(String[] args) {
+        Socket socket;
+        try {
+            socket = new Socket("localhost", 7777);
+
+            System.out.println("서버에 연결되었습니다.");
+
+            Sender sender = new Sender(socket);
+            Receiver receiver = new Receiver(socket);
+
+            sender.start();
+            receiver.start();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
